d507c0dbacf49ccab45a0cfa9e1cff7283153195
the-smooth-operator <alberto.delbarrio.albelda@gmail.com>
2020-05-22 20:01:58 +0200

    rename to liveco2.com

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import emailIcon from './gmail.png';
 function App() {
   return (
     <div className="body">
-        <h1 className="mainTitle">THE CO2 FIGHT</h1>
+        <h1 className="mainTitle">Live CO2</h1>
         <div className="intro">
-            <p><b>The CO2 Fight</b> nace en 2019 con el objetivo de valorar los árboles por su absorción de carbono.</p>
+            <p><b>Live CO2</b> nace en 2019 con el objetivo de valorar los árboles por su absorción de carbono.</p>
             <p>Para ello queremos <b>cuantificar, certificar y poner a disposición del mercado</b> las toneladas de CO2 absorbidas por los bosques.</p>
             <div className="spacedText">
                 <p>Actualmente nuestro proyecto <b>esta en fase de desarrollo</b>, por lo que agradecemos tus ideas y sugerencias.</p>
@@ -18,13 +18,13 @@ function App() {
             </div>
             <p className="subTitle">Contacta con nosotros</p>
             <div className="contact">
-              <a href="https://instagram.com/theco2fight?igshid=1bgtikum9cr66" target="_blank" rel="noopener noreferrer">
+              <a href="https://instagram.com/liveco2?igshid=1bgtikum9cr66" target="_blank" rel="noopener noreferrer">
                 <img className="social-link" src={instagramIcon} alt="instagramIcon" />
               </a>
-                <a href="https://www.facebook.com/theco2fight/" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.facebook.com/liveco2/" target="_blank" rel="noopener noreferrer">
                     <img className="social-link" src={facebookIcon} alt="facebookIcon" />
                 </a>
-                <a href="mailto:admin@theco2fight.com"><img className="social-link" src={emailIcon} alt="envelopeIcon" /></a>
+                <a href="mailto:admin@liveco2.com"><img className="social-link" src={emailIcon} alt="envelopeIcon" /></a>
             </div>
         </div>
     </div>
